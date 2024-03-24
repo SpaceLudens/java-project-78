fca318f67f61a2bfb1fd14c532c14410c2b98930
SpaceLudens <SpaceLuden1@gmail.com>
2024-03-24 12:48:13 +0300

    add example

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class Validator {
     public static void main(String[] args) {
-        System.out.println("validatorTest");
+        System.out.println("test");
     }
 }
