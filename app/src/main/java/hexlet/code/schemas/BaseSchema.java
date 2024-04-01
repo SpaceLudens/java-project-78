1aaab15c776ec85683737d34f008ab307341635d
SpaceLudens <SpaceLuden1@gmail.com>
2024-04-01 16:12:37 +0300

    refactoring

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,22 +6,17 @@ import java.util.Map;
 import java.util.function.Predicate;
 
 public class BaseSchema<T> {
-    private Map<String, Predicate<T>> predicates;
+    private final Map<String, Predicate<T>> predicates;
 
-    public Map<String, Predicate<T>> getPredicates() {
+    public final Map<String, Predicate<T>> getPredicates() {
         return predicates;
     }
-
-    public void setPredicates(Map<String, Predicate<T>> predicates) {
-        this.predicates = predicates;
-    }
-
 
     public BaseSchema() {
         this.predicates = new HashMap<>();
     }
 
-    public  boolean isValid(T value) {
+    public boolean isValid(T value) {
         Collection<Predicate<T>> values = predicates.values();
         return values.stream().allMatch(p -> p.test(value));
     }
