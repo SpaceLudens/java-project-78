925fd259267b6e1b8f62d8117dd5bf26a2d376d8
SpaceLudens <SpaceLuden1@gmail.com>
2024-04-02 12:22:05 +0300

    updated MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -26,23 +26,44 @@ public final class MapSchema extends BaseSchema<Map<String, String>> {
         return this;
     }
 
-    @Override
+    /**
+     * Проверяет, соответствует ли переданная карта значений схеме, определенной для данного объекта MapSchema.
+     * <p>
+     * Метод сначала проверяет базовую валидацию, используя реализацию метода isValid() родительского класса. Затем он
+     * проверяет каждую схему, определенную для объекта MapSchema, и проверяет, содержит ли переданная карта всех
+     * необходимых ключей и соответствует ли каждое значение схеме для соответствующего ключа.
+     * </p>
+     *
+     * @param value карта значений, которую необходимо проверить на соответствие схеме
+     * @return true, если карта значений соответствует схеме; в противном случае - false
+     */
     public boolean isValid(Map<String, String> value) {
+        // Проверяем базовую валидацию
         if (!super.isValid(value)) {
-            return false;
+            return false; // Ранний выход, если базовая валидация не прошла
         }
+
+        // Проверяем соответствие структуре, если схема определена
         if (schemas != null) {
+            // Проверяем каждую схему для ключей в схеме MapSchema
             return schemas.entrySet().stream()
                     .allMatch(entry -> {
                         String key = entry.getKey();
                         BaseSchema<String> schema = entry.getValue();
+
+                        // Проверяем, содержит ли входное значение все необходимые ключи
                         if (!value.containsKey(key)) {
                             return false;
                         }
+
                         String val = value.get(key);
+
+                        // Проверяем, что значение строки не равно null, прежде чем вызывать метод length()
                         return val != null && schema.isValid(val);
                     });
         }
+
         return true;
     }
+
 }
